64ce2f98ff60c2a081d194646bd8ad67e72d1fe0
Vitor Patricio Chaves <vitor.patricio.chaves@gmail.com>
2022-09-12 21:27:09 -0300

    Edits banners

--- src/components/banner-bottom/index.js
+++ src/components/banner-bottom/index.js
@@ -3,15 +3,9 @@ import './styles.css'
 
 const BannerBottom = () => {
   return (
-    <div className="banner-bottom">
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12">
-            <div className="banner-head">
-                <h2>Onde nascem as lendas</h2>
-            </div>
-          </div>
-        </div>
+    <div className="container">
+      <div className="banner-bottom">
+        {/* <h2>Onde nascem as lendas</h2> */}
       </div>
     </div>
   )
--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -3,15 +3,9 @@ import './styles.css'
 
 const Banner = () => {
   return (
-    <div className="banner bg-with-black">
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12">
-            <div className="banner-head">
-                <h2>A alma dos craques</h2>
-            </div>
-          </div>
-        </div>
+    <div className="container">
+      <div className="banner">
+        {/* <h2>A alma dos craques</h2> */}
       </div>
     </div>
   )
